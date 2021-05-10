c7b8f901716ec4e2b747d0b8c9bf1f48e9dd3ffc
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-05-10 04:07:46 +0200

    export

---
+++ index.js
@@ -0,0 +1,7 @@
+const { RandomRoundRobin } = require('./src/RandomRoundRobin');
+const { SequentialRoundRobin } = require('./src/SequentialRoundRobin');
+
+module.exports = {
+  RandomRoundRobin,
+  SequentialRoundRobin
+};
