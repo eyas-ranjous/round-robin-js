3e1969aa7a0c64889da468a5a19bc8958d5e7839
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-06-24 10:56:59 +0200

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { RandomRoundRobin } from './src/RandomRoundRobin';
+import { SequentialRoundRobin } from './src/SequentialRoundRobin';
+
+export { RandomRoundRobin }
+export { SequentialRoundRobin }
