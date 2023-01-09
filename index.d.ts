3a7a6b5ba05ead241319061cc7f2ba37a31ce764
Eyas Ranjous <eyas.ranjous@gmail.com>
2023-01-09 20:29:42 -0300

    add RoundRobinItem

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
+import { RoundRobinItem } from './src/RandomRoundRobin';
 import { RandomRoundRobin } from './src/RandomRoundRobin';
 import { SequentialRoundRobin } from './src/SequentialRoundRobin';
 import { PriorityRoundRobin } from './src/PriorityRoundRobin';
 
+export { RoundRobinItem }
 export { RandomRoundRobin }
 export { SequentialRoundRobin }
 export { PriorityRoundRobin }
